b98111fe621fe394f87a94229bda8aa732f2a6da
horrorjoy <hashm19927@gmail.com>
2023-03-04 22:44:16 +0100

    Homework7

---
+++ Задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+double[,] array =new double[3,4];
+
+for (int i=0; i<array.GetLength(0);i++) // строки
+{
+    for (int j=0; j<array.GetLength(0);j++) //столбцы
+    {
+        array[i,j]=Convert.ToDouble(new Random().Next(10))/10;
+        Console.Write($"{array[i,j]}");
+    }
+    Console.WriteLine();
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [5,5];
+
+for (int i=0; i<numbers.GetLength(0);i++)
+{
+    for (int j=0; j<numbers.GetLength(0);j++)
+    {
+        numbers[i,j]=new Random().Next(10);
+        Console.Write($"{numbers[i,j]+" "}");
+    }
+    Console.WriteLine();
+}
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.Write($"Строка -> {n},столбец -> {m} = элемент {numbers[n-1,m-1]}");
+}
